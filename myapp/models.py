0c52f81fcaf58591ab0ccf85dd1e657c006e4136
ramansain20 <20je0766@ece.iitism.ac.in>
2023-07-18 12:13:07 +0530

    frontend improved

--- myapp/models.py
+++ myapp/models.py
@@ -3,6 +3,12 @@ from PIL import Image
 import numpy as np
 # Create your models here.
 class FloorImage(models.Model):
+    username=models.CharField(max_length=100 ,blank=False,default="")
     name = models.CharField(max_length=50,blank=True,default="")
     image = models.ImageField(upload_to='uploads/')
+
+class User(models.Model):
+    username=models.CharField(max_length=100)
+    data=models.JSONField(null=True,blank=True)
+    
 
--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse,JsonResponse
 from django.conf import settings
 from . import models
 from .utils import util
-import json
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
@@ -12,10 +11,15 @@ def upload(request):
     print(request.POST)
     if request.method == "POST":
         name = request.POST.get("name")
+        username = request.POST.get("username")
         image = request.FILES["image"]
-        floorimage = models.FloorImage(name=name,image=image)
+        floorimage = models.FloorImage(name=name,image=image,username=username)
         floorimage.save()
-        data=util(image_path = settings.MEDIA_ROOT + "/"+str(floorimage.image) )
+        data=util(image_path = settings.MEDIA_ROOT + "/"+str(floorimage.image))
+        user=models.User(username=username,data=data)
+        user.save()
+        print(user)
+
     return JsonResponse(data,safe=False)
 
 
